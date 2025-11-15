9c9f2e778dbb4048be964e7f1a5de475cfb7e336
prabhnoor12 <singhrajbirsekhon80@outlook.com>
2025-11-15 11:22:10 +0400

    add

--- backend/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-// Load environment variables for Prisma CLI because automatic .env loading is skipped when using prisma.config.ts
-// See: https://pris.ly/prisma-config-env-vars
-import 'dotenv/config';
-
-export default {
-  schema: './prisma/schema.prisma',
-};
